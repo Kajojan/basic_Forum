cd8284eed886e7a8a150098ced29ceab57cf27ff
Kajojan <kojoty1999@gmail.com>
2023-08-26 21:53:33 +0200

    change project

--- movie_library/src/test/java/pl/movielibrary/movie_library/MovieLibraryApplicationTests.java
+++ movie_library/src/test/java/pl/movielibrary/movie_library/MovieLibraryApplicationTests.java
@@ -1,13 +1,13 @@
-package com.movie.movie_library;
+package pl.movielibrary.movie_library;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiSqlApplicationTests {
+class MovieLibraryApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
