80e92d277ebb96117162d0021be17bac1db7173c
Kajojan <kojoty1999@gmail.com>
2023-08-29 12:35:14 +0200

    MovieRepository class + getAll func & getByID func

--- movie_library/src/main/java/pl/movielibrary/movie_library/movieController.java
+++ movie_library/src/main/java/pl/movielibrary/movie_library/movieController.java
@@ -1,12 +1,28 @@
 package pl.movielibrary.movie_library;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class movieController {
+    @Autowired
+    MovieRepository movieRepository;
+
     @GetMapping("/test")
     public int test(){
         return 1;
+    }
+    @GetMapping("/movies")
+    public List<Movie> getAll(){
+        return movieRepository.getAll();
+
+    }
+    @GetMapping("/movies/{id}")
+    public Movie getById(@PathVariable("id") int id){
+        return movieRepository.getById(id);
     }
 }
