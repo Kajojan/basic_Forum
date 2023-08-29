e486308233e556840bffee461f6a0dfb3ccbd4c0
Kajojan <kojoty1999@gmail.com>
2023-08-29 19:34:13 +0200

    add movie endPoint & update movie endPoint

--- movie_library/src/main/java/pl/movielibrary/movie_library/movieController.java
+++ movie_library/src/main/java/pl/movielibrary/movie_library/movieController.java
@@ -1,13 +1,12 @@
 package pl.movielibrary.movie_library;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class movieController {
     @Autowired
     MovieRepository movieRepository;
@@ -16,13 +15,31 @@ public class movieController {
     public int test(){
         return 1;
     }
-    @GetMapping("/movies")
+    @GetMapping("")
     public List<Movie> getAll(){
         return movieRepository.getAll();
-
     }
-    @GetMapping("/movies/{id}")
+    @GetMapping("/{id}")
     public Movie getById(@PathVariable("id") int id){
         return movieRepository.getById(id);
     }
+
+    @PostMapping("")
+    public int addMovie(@RequestBody  List<Movie> movies){
+        return movieRepository.save(movies);
+    }
+
+    @PutMapping("/{id}")
+    public int updateMovie(@PathVariable("id") int id, @RequestBody Movie updatedMovie){
+        Movie movie = movieRepository.getById(id);
+        if(movie != null){
+            movie.setMovie_title(updatedMovie.getMovie_title());
+            movie.setRating(updatedMovie.getRating());
+            movieRepository.update(movie);
+            return 1;
+        }else {
+            return -1;
+        }
+    }
+
 }
